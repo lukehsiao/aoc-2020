ebd1c14dfcaffe84bdbec96217a43c032ea79f4f
Luke Hsiao <lwhsiao@stanford.edu>
2020-12-01 13:47:39 -0700

    01: refactor twosum to be used by 3sum

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,33 +1,41 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::{self, Read};
 
 use anyhow::Result;
 
-const TARGET: i64 = 2020;
-
 // This is just TwoSum
-fn part1(input: &str) -> Result<()> {
-    let mut map = HashMap::new();
+fn part1(nums: &Vec<i64>, target: i64) -> Result<HashSet<i64>> {
+    let mut dual: HashSet<i64> = HashSet::new();
+    let mut result: HashSet<i64> = HashSet::new();
 
-    let mut result = 0;
-
-    for line in input.lines() {
-        let num: i64 = line.parse()?;
-        if map.contains_key(&num) {
-            result = num * map.get(&num).unwrap();
+    for num in nums {
+        if dual.contains(num) {
+            result.insert(*num);
+            result.insert(target - num);
+            break;
         } else {
-            map.insert(TARGET - num, num);
+            dual.insert(target - num);
         }
     }
 
-    println!("Part 1: {}", result);
+    Ok(result)
+}
+
+// This is just ThreeSum
+fn part2(nums: &mut Vec<i64>) -> Result<()> {
+    let result = 0;
+    nums.sort();
+
+    println!("Part 2: {}", result);
     Ok(())
 }
 
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let mut nums: Vec<i64> = input.lines().map(|n| n.parse::<i64>().unwrap()).collect();
 
-    part1(&input)?;
+    println!("Part 1: {}", part1(&nums, 2020)?.iter().product::<i64>());
+    part2(&mut nums)?;
     Ok(())
 }
