06142db3757c47b257d2e1f5d69d7c09ae92ee0f
Luke Hsiao <lwhsiao@stanford.edu>
2020-12-01 13:25:59 -0700

    01: implement part 1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::io::{self, Read};
+
+use anyhow::Result;
+
+const TARGET: i64 = 2020;
+
+// This is just TwoSum
+fn part1(input: &str) -> Result<()> {
+    let mut map = HashMap::new();
+
+    let mut result = 0;
+
+    for line in input.lines() {
+        let num: i64 = line.parse()?;
+        if map.contains_key(&num) {
+            result = num * map.get(&num).unwrap();
+        } else {
+            map.insert(TARGET - num, num);
+        }
+    }
+
+    println!("Part 1: {}", result);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    Ok(())
 }
