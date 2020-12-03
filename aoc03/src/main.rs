039f4364e8acb6fc369bd072f6e252e1728c2c25
Luke Hsiao <lwhsiao@stanford.edu>
2020-12-03 11:03:19 -0700

    03: implement part 1

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::io::{self, Read};
 use std::str::FromStr;
 
@@ -14,10 +13,11 @@ impl FromStr for Slope {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Slope> {
-        let mut trees: Vec<Vec<bool>> = vec![vec![]];
+        let mut trees: Vec<Vec<bool>> = vec![];
         for line in s.lines() {
             let mut row: Vec<bool> = vec![];
             for c in line.chars() {
+                // If there is a tree, it is true
                 match c {
                     '.' => row.push(false),
                     '#' => row.push(true),
@@ -30,12 +30,61 @@ impl FromStr for Slope {
     }
 }
 
+impl Slope {
+    fn iter_part1(&self) -> IterPart1 {
+        IterPart1 {
+            slope: self,
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+struct IterPart1<'a> {
+    slope: &'a Slope,
+    x: usize,
+    y: usize,
+}
+
+impl<'a> Iterator for IterPart1<'a> {
+    type Item = bool;
+
+    fn next(&mut self) -> Option<bool> {
+        let result = if let Some(next_row) = self.slope.trees.get(self.y) {
+            // Wrap next-row if necessary
+            let x = match self.x {
+                0 => 0,
+                n => n % next_row.len(),
+            };
+            if next_row[x] {
+                Some(true)
+            } else {
+                Some(false)
+            }
+        } else {
+            None
+        };
+
+        self.y += 1;
+        self.x += 3;
+
+        result
+    }
+}
+
+fn part1(slope: &Slope) -> Result<()> {
+    let count: u32 = slope.iter_part1().map(|b| b as u32).sum();
+
+    println!("Part 1: {}", count);
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-
     let slope: Slope = input.parse()?;
-    dbg!(slope);
+
+    part1(&slope)?;
 
     Ok(())
 }
