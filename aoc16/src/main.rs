3cfd95e52528ea706fea431b3fb3a944abb13329
Luke Hsiao <lwhsiao@stanford.edu>
2020-12-15 23:17:07 -0800

    16: refactor: add Rule struct and part2 placeholders

--- aoc16/src/main.rs
+++ aoc16/src/main.rs
@@ -6,30 +6,47 @@ use anyhow::Result;
 use lazy_static::lazy_static;
 use regex::Regex;
 
+#[derive(Debug)]
+struct Rule {
+    lower: RangeInclusive<usize>,
+    upper: RangeInclusive<usize>,
+}
+
+impl Rule {
+    fn contains(&self, n: usize) -> bool {
+        self.lower.contains(&n) || self.upper.contains(&n)
+    }
+}
+
+fn parse_rules(input: &str) -> Result<Vec<Rule>> {
+    Ok(input
+        .lines()
+        .map(|l| {
+            lazy_static! {
+                static ref RULE: Regex =
+                    Regex::new(r"^(?P<name>[\w\s]+):\s+(?P<r1>[\d-]+) or (?P<r2>[\d-]+)").unwrap();
+            }
+
+            let caps = match RULE.captures(l) {
+                None => panic!("No match: {}", l),
+                Some(c) => c,
+            };
+
+            let r1_parts: Vec<usize> = caps["r1"].split('-').map(|s| s.parse().unwrap()).collect();
+            let lower = r1_parts[0]..=r1_parts[1];
+
+            let r2_parts: Vec<usize> = caps["r2"].split('-').map(|s| s.parse().unwrap()).collect();
+            let upper = r2_parts[0]..=r2_parts[1];
+
+            Rule { lower, upper }
+        })
+        .collect())
+}
+
 fn part1(input: &str) -> Result<usize> {
     let sections: Vec<_> = input.split("\n\n").collect();
 
-    let rules: Vec<RangeInclusive<usize>> = sections[0].lines().fold(vec![], |mut ranges, l| {
-        lazy_static! {
-            static ref RULE: Regex =
-                Regex::new(r"^(?P<name>[\w\s]+):\s+(?P<r1>[\d-]+) or (?P<r2>[\d-]+)").unwrap();
-        }
-
-        let caps = match RULE.captures(l) {
-            None => panic!("No match: {}", l),
-            Some(c) => c,
-        };
-
-        let r1_parts: Vec<usize> = caps["r1"].split('-').map(|s| s.parse().unwrap()).collect();
-        let range_1 = r1_parts[0]..=r1_parts[1];
-        ranges.push(range_1);
-
-        let r2_parts: Vec<usize> = caps["r2"].split('-').map(|s| s.parse().unwrap()).collect();
-        let range_2 = r2_parts[0]..=r2_parts[1];
-        ranges.push(range_2);
-
-        ranges
-    });
+    let rules = parse_rules(sections[0])?;
 
     // Check all nearby tickets only
     let ticket_error_rate = sections[2].lines().skip(1).fold(0, |mut acc, ticket| {
@@ -38,7 +55,7 @@ fn part1(input: &str) -> Result<usize> {
             .split(',')
             .map(|v| v.parse::<usize>().unwrap())
         {
-            if rules.iter().all(|range| !range.contains(&value)) {
+            if rules.iter().all(|rule| !rule.contains(value)) {
                 acc += value
             }
         }
@@ -46,6 +63,16 @@ fn part1(input: &str) -> Result<usize> {
     });
 
     Ok(ticket_error_rate)
+}
+
+fn part2(input: &str) -> Result<usize> {
+    let sections: Vec<_> = input.split("\n\n").collect();
+
+    let rules = parse_rules(sections[0])?;
+
+    dbg!(&rules);
+
+    todo!()
 }
 
 fn main() -> Result<()> {
@@ -56,6 +83,12 @@ fn main() -> Result<()> {
     let now = Instant::now();
     match part1(&input) {
         Ok(v) => println!("Part 1: {}, took {:#?}", v, now.elapsed()),
+        Err(e) => eprintln!("{}", e),
+    };
+
+    let now = Instant::now();
+    match part2(&input) {
+        Ok(v) => println!("Part 2: {}, took {:#?}", v, now.elapsed()),
         Err(e) => eprintln!("{}", e),
     };
 
